908deda121e1bc0ccf8d387c41729c3429197fe6
Matthias Gelszinnis <matthias.gelszinnis@gmail.com>
2021-12-15 18:33:05 +0100

    update

--- shortener.go
+++ shortener.go
@@ -8,6 +8,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type ShortLink struct {
@@ -15,30 +16,30 @@ type ShortLink struct {
 	Link string
 }
 
-var db sql.DB
+var Datenbank *sql.DB
 
 func main() {
-	db, err := sql.Open("sqlite3", "./shortener.db")
+	var err error
+	Datenbank, err = sql.Open("sqlite3", "./shortener.db")
 	checkErr(err)
-	defer db.Close()
+	//defer Datenbank.Close()
 
 	var tablename string
 	sqlInitCheck := "SELECT tbl_name FROM sqlite_master WHERE type='table' AND name='links';"
-	err = db.QueryRow(sqlInitCheck).Scan(&tablename)
-	checkErr(err)
+	err = Datenbank.QueryRow(sqlInitCheck).Scan(&tablename)
 
 	if err == sql.ErrNoRows {
-		sqlInit := "CREATE TABLE links (id TEXT not null primary key, link TEXT);"
-		_, err = db.Exec(sqlInit)
+		sqlInit := "CREATE TABLE links (id INTEGER not null primary key autoincrement, link TEXT);"
+		_, err = Datenbank.Exec(sqlInit)
 		checkErr(err)
 		sqlIndex := "CREATE UNIQUE INDEX idx_links_id ON links(id);"
-		_, err = db.Exec(sqlIndex)
+		_, err = Datenbank.Exec(sqlIndex)
 		checkErr(err)
 	}
 
 	r := mux.NewRouter()
 	r.HandleFunc("/createlink", linkCreator)
-	r.HandleFunc("/{shorturl}", rerouteHandler)
+	r.HandleFunc("/{shorturl:[A-Za-z0-9]+}", rerouteHandler)
 	r.HandleFunc("/", pageHandler)
 
 	srv := &http.Server{
@@ -55,10 +56,10 @@ func pageHandler(w http.ResponseWriter, r *http.Request) {
 
 func linkCreator(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	longurl := r.Form["longurl"]
+	longurl := strings.Join(r.Form["longurl"], "")
 	fmt.Printf("übereichte lange URL: %v\n", longurl)
 
-	sqlInsert, err := db.Prepare("INSERT INTO links (link) VALUES (?);")
+	sqlInsert, err := Datenbank.Prepare("INSERT INTO links (link) VALUES (?);")
 	checkErr(err)
 	result, err := sqlInsert.Exec(longurl)
 	checkErr(err)
@@ -67,7 +68,9 @@ func linkCreator(w http.ResponseWriter, r *http.Request) {
 	checkErr(err)
 	fmt.Printf("Zurückgegebene ID für die URL %v : %v\n", longurl, returnedId)
 
-	fertigerLink := fmt.Sprintf("%v/%v", r.URL, returnedId)
+	fertigerLink := fmt.Sprintf("%v/%v", r.Host, returnedId)
+	fmt.Printf("Der fertige Link %v\n", fertigerLink)
+	fmt.Printf("Der Hostname: %v \n", r.Host)
 	t, _ := template.ParseFiles("createlink.gohtml")
 	link := ShortLink{int(returnedId), fertigerLink}
 	t.Execute(w, link)
@@ -77,18 +80,14 @@ func linkCreator(w http.ResponseWriter, r *http.Request) {
 func rerouteHandler(w http.ResponseWriter, r *http.Request) {
 
 	path := mux.Vars(r)
-	sqlQuery, err := db.Prepare("SELECT link FROM links WHERE id = ?;")
+	sqlQuery, err := Datenbank.Prepare("SELECT link FROM links WHERE id = ?;")
 	checkErr(err)
 
-	result, err := sqlQuery.Query(path["shorturl"])
+	var result string
+	err = sqlQuery.QueryRow(path["shorturl"]).Scan(&result)
 	checkErr(err)
 
-	var dbResult string
-	err = result.Scan(&dbResult)
-	checkErr(err)
-	result.Close()
-
-	http.Redirect(w, r, dbResult, 302)
+	http.Redirect(w, r, result, 302)
 }
 
 func checkErr(err error) {
